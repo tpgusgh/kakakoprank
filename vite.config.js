a9e6c3a3ca13ef3d49d43d300450d4145111ae83
hyunho <kimhyunhi1@gmail.com>
2025-04-10 12:27:27 +0900

    connect kakao && env

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsx: 'automatic', // JSX 자동 변환 설정
+  },
   define: {
     'process.env.VITE_KAKAO_API_KEY': JSON.stringify(process.env.VITE_KAKAO_API_KEY_KAKAO_API_KEY),
   },
